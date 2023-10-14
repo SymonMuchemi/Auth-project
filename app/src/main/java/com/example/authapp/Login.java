a46c2c14e8f5bf7d611c76e7ade7e132f47c7efe
SymonMuchemi <simonkenya846@gmail.com>
2023-10-14 16:12:29 +0300

    fix: set string values

--- app/src/main/java/com/example/authapp/Login.java
+++ app/src/main/java/com/example/authapp/Login.java
@@ -2,13 +2,38 @@ package com.example.authapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Objects;
 
 public class Login extends AppCompatActivity {
+    TextInputEditText email, password;
+    Button loginBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Bind the TextInputEditTexts and Button to their respective views
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.login_button);
+
+        loginBtn.setOnClickListener(v -> {
+            // Get the text from the TextInputEditTexts
+            String emailText = Objects.requireNonNull(email.getText()).toString();
+            String passwordText = Objects.requireNonNull(password.getText()).toString();
+
+            // create intent to go to the activity_registration activity
+            Intent intent = new Intent(Login.this, Registration.class);
+            //start intent
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/authapp/Registration.java
+++ app/src/main/java/com/example/authapp/Registration.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class Registration extends AppCompatActivity {
 
+    TextInputEditText email, password, confirmPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
